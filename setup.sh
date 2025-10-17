30064d3daaf27b58278555e755d61baebb8b3a03
crpruett <crpruett@iu.edu>
2025-10-17 17:27:11 -0400

    adding zsh and tmux

--- setup.sh
+++ setup.sh
@@ -3,9 +3,22 @@
 set -euo pipefail
 XDG_CONFIG_HOME="$HOME/.config"
 
+if command -v zsh > /dev/null; then 
+	sudo chsh -s "$(command 0v zsh)" "$USER"
+fi
+
 if ! command -v chezmoi >/dev/null; then
 	sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:crpruett/dotfiles.git
 fi
+
+if [ ! -d "$HOME/.zsh" ]; then
+	mkdir -p "$HOME/.zsh"
+fi
+
+if [ ! -d "$HOME/.zsh/pure" ]; then
+	git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+fi
+
 
 #sudo apt update
 
