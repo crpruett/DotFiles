86d26d9e2ad903ed8d17164cb17d8dc815b74679
crpruett <crpruett@iu.edu>
2025-10-22 20:19:40 +0000

    lazyvim extras

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+	{ "echasnoviski/mini.pairs", enabled = false },
+	{ "folke/noice.nvim", enabled = false },
+	{ "rcarriga/nvim-notify", enabled = false },
+}
---
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,5 @@
+return {
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazivim.plugins.extras.lang.toml" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+}
